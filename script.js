4032beca639a17ff4d261af8192f3482dc91c7ad
JuanVergara-9 <138065862+JuanVergara-9@users.noreply.github.com>
2024-01-23 18:03:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 peliculasNuevas.forEach(img => {
     img.addEventListener('click', function() {
-        // Cambia la imagen (simplemente como ejemplo)
         img.src = 'nueva-ruta-imagen.jpg';
 
-        // Puedes agregar lógica adicional aquí para personalizar la animación
-        img.classList.remove('zoomIn'); // Elimina la clase existente
-        void img.offsetWidth; // Reinicia el reflow para aplicar la nueva clase
-        img.classList.add('zoomIn'); // Agrega la clase de nuevo
+        img.classList.remove('zoomIn'); 
+        void img.offsetWidth;
+        img.classList.add('zoomIn'); 
     });
 });
 
